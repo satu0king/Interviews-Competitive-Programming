161b18253146e769467f3c0340458f117a2596fd
Satvik Ramaprasad <satviksr@gmail.com>
2019-07-28 16:08:11 +0530

    bit update

--- bit.cpp
+++ bit.cpp
@@ -1,13 +1,16 @@
-struct bit{		// use 1 based indices!!!
+struct bit{		// use 0 based indices!!!
 	int n; vector<int> tree;
 	bit(int n) : n(n) { tree.assign(n + 1, 0); }
 	int query(int l, int r) { return query(r) - query(l - 1); }
 	int query(int r) {
-		int s = 0;
-		for(; r > 0; r -= (r & (-r))) s += tree[r];
-		return s;
+        int ans = 0;
+        for(r++; r; r -= r&(-r)) ans+=tree[r];
+        return ans;
 	}
 	void update(int i, int v) {
-		for(; i <= n; i += (i & (-i))) tree[i] += v;
+       for(i++; i <= n; i+= i&(-i)) tree[i]+=v;
+	}
+	void assign(int i, int v) {
+       update(i, v - query(i, i));
 	}
 };
