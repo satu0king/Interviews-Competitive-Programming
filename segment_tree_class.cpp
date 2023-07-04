a33558aeacc4ccbd5247eb3f921ffbfc518f079b
Satvik Ramaprasad <satviksr@gmail.com>
2023-07-04 15:31:28 +0530

    Update segment_tree_class.cpp

--- segment_tree_class.cpp
+++ segment_tree_class.cpp
@@ -1,63 +1,102 @@
-#include <bits/stdc++.h>
-using namespace std;
+// https://leetcode.com/problems/falling-squares
 
-// Range Max Query
+struct segment_tree {
+    vector<int> tree;
+    vector<int> lazy;
+    int n;
 
-// Range Max Query
-struct SegmentTree {
-  vector<int> tree;
-  vector<int> lazy;
-  int n;
-  SegmentTree(int n) : n(n) {
-    int size = 1;
-    while (size <= n)
-      size *= 2;
-    size *= 2;
-    tree.resize(size);
-    lazy.resize(size);
-  }
-
-  int combine(int l, int r) { return max(l, r); }
-
-  void pushDown(int id) {
-    if (!lazy[id])
-      return;
-    lazy[2 * id] += lazy[id];
-    lazy[2 * id + 1] += lazy[id];
-    tree[2 * id] += lazy[id];
-    tree[2 * id + 1] += lazy[id];
-    lazy[id] = 0;
-  }
-
-  int get(int id, int tl, int tr, int l, int r) {
-    if (l > r)
-      return INT_MIN;
-    if (tl == l && tr == r)
-      return tree[id];
-    pushDown(id);
-    int treeMid = (tl + tr) / 2;
-    int lValue = get(id * 2, tl, treeMid, l, min(treeMid, r));
-    int rValue = get(id * 2 + 1, treeMid + 1, tr, max(l, treeMid + 1), r);
-    return combine(lValue, rValue);
-  }
-
-  int get(int l, int r) { return get(1, 0, n - 1, l, r); }
-
-  void update(int id, int tl, int tr, int l, int r, int value) {
-    if (l > r)
-      return;
-    if (tl == l && tr == r) {
-      tree[id] += value;
-      lazy[id] += value;
-      return;
+    segment_tree(int n) {
+        int size = 1;
+        while(size < n) {
+            size *= 2;
+        }
+        n = size;
+        tree.resize(2 * size);
+        lazy.assign(size, -1);
+        this->n = n;
     }
-    pushDown(id);
-    int treeMid = (tl + tr) / 2;
-    update(id * 2, tl, treeMid, l, min(treeMid, r), value);
-    update(id * 2 + 1, treeMid + 1, tr, max(l, treeMid + 1), r, value);
-    tree[id] = combine(tree[id * 2], tree[id * 2 + 1]);
-  }
 
-  void update(int l, int r, int value) { update(1, 0, n - 1, l, r, value); }
-  void update(int i, int value) { update(i, i, value); }
+    // [x, y)
+    int query(int x, int y) {
+        return query(x, y, 1, 0, n);
+    }
+
+    // point update
+    void update(int x, int val) {
+        update(x, val, 1, 0, n);
+    }
+
+    // range update [x, y)
+    void update(int x, int y, int val) {
+        update(x, y, val, 1, 0, n);
+    }
+
+    private:
+    // point update
+    void update(int x, int val, int id, int l, int r) {
+        if (l + 1 == r) {
+            tree[id] = val;
+            return;
+        }
+        int mid = (l + r) / 2;
+        if (x < mid) update(x, val, left(id), l, mid);
+        else update(x, val, right(id), mid, r);
+
+        tree[id] = combine(tree[left(id)], tree[right(id)]);
+    }
+
+    void update_lazy(int id, int val) {
+        tree[id] = val;
+        if (id < n)
+            lazy[id] = val;
+    }
+
+    void update(int x, int y, int val, int id, int l, int r) {
+        // fully exclusive
+        if (y <= l || r <= x) return;
+        
+        // fully inclusive
+        if (x <= l && r <= y) {
+            update_lazy(id, val);
+            return;
+        }
+
+        propagate(id);
+
+        int m = (l + r) / 2;
+        update(x, y, val, left(id), l, m);
+        update(x, y, val, right(id), m, r);
+
+        tree[id] = combine(tree[left(id)], tree[right(id)]);
+    }
+
+    void propagate(int id) {
+        if (id < n && ~lazy[id]) {
+            update_lazy(left(id), lazy[id]);
+            update_lazy(right(id), lazy[id]);
+            lazy[id] = -1;
+        }
+    }
+
+    int left(int id) {
+        return id * 2;
+    }
+    int right(int id) {
+        return id * 2 + 1;
+    }
+
+    int query( int x, int y, int id, int l, int r) {
+        if (x <= l && r <= y) return tree[id];
+        if (y <= l || r <= x) return 0;
+
+        propagate(id);
+
+        int mid = (l + r) / 2;
+
+        return combine(query(x, y, left(id), l, mid), query(x, y, right(id), mid, r));
+    }
+
+    int combine(int a, int b) {
+        return max(a, b);
+    }
 };
